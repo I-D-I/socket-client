61d4790fd3f80c47f8851b5765d4898a0b6e62b0
Ginés Collado <gines.collado@gmail.com>
2022-04-29 07:53:10 +0200

    final

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Socket } from 'ngx-socket-io';
+import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +9,10 @@ import { Socket } from 'ngx-socket-io';
 export class WebsocketService {
 
   public socketStatus = false;
+  public usuario: Usuario = new Usuario('');
 
-  constructor(private socket: Socket) {
+  constructor(private socket: Socket, private router: Router) {
+    this.cargarStorage();
     this.checkStatus();
   }
 
@@ -16,6 +20,7 @@ export class WebsocketService {
     this.socket.on('connect', () => {
       console.log('Conectado al servidor');
       this.socketStatus = true;
+      this.cargarStorage();
     });
 
     this.socket.on('disconnect', () => {
@@ -31,6 +36,42 @@ export class WebsocketService {
 
   listen( evento: string ) {
     return this.socket.fromEvent( evento );
+  }
+
+  loginWS( nombre: string ) {
+    return new Promise<void>( (resolve, reject ) => {
+      this.emit('configurar-usuario', { nombre: nombre }, (resp: any) => {
+        this.usuario = new Usuario( nombre );
+        this.guardarStorage();
+        resolve();
+      });
+    });
+  }
+
+  logoutWS() {
+    this.usuario = {} as Usuario;
+    localStorage.removeItem('usuario-chat');
+    const payload = {
+      nombre: 'sin-nombre'
+    }
+    this.emit('configurar-usuario', payload, () => {});
+    this.router.navigateByUrl('');
+  }
+
+  getUsuario() {
+    return this.usuario;
+  }
+
+  guardarStorage() {
+    localStorage.setItem('usuario-chat', JSON.stringify( this.usuario ));
+  }
+
+  cargarStorage() {
+    let usuario = localStorage.getItem('usuario-chat');
+    if( usuario ) {
+      this.usuario = JSON.parse( usuario );
+      this.loginWS ( this.usuario.nombre );
+    }
   }
 
 }
